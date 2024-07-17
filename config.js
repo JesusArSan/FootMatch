2f0a6f16125623bae3cf5a01af7e6b9d19b20ea6
jesusarsan <jesus02@correo.ugr.es>
2024-07-17 17:10:14 +0200

    Status Bar corregido

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 const config = {
 	// serverUrl: "http://192.168.1.56:3000", // URL
 	// serverUrl: "http://37.11.76.170:3000", // URL
-	// serverUrl: "http://79.117.112.176:3000", // URL
-	serverUrl: "http://192.168.1.33:3000", // URL
+	serverUrl: "http://79.116.62.91:3000", // URL
+	// serverUrl: "http://192.168.1.33:3000", // URL
 };
 
 export default config;
